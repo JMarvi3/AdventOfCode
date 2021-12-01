7e673dce16bf80b8aab29c94549362963d8d221e
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2021-12-01 11:19:39 -0500

    Completed 2021 day 1 (revised).

--- 2021/1.py
+++ 2021/1.py
@@ -1,19 +1,21 @@
 from current_puzzle import current_puzzle
 
+
+def increasing_count(data):
+    return sum(data[i] > data[i - 1] for i in range(1, len(data)))
+
+
 puzzle = current_puzzle()
 input_data = puzzle.input_data
-
-
-increasing_count = lambda data: sum(data[i] > data[i-1] for i in range(1, len(data)))
-
 depths = list(map(int, input_data.splitlines()))
+
 puzzle.answer_a = increasing_count(depths)
 print('Part1:', puzzle.answer_a)
 
-sums = list(sum(depths[i:i+3]) for i in range(len(depths)-2))
+sums = list(sum(depths[i:i + 3]) for i in range(len(depths) - 2))
 puzzle.answer_b = increasing_count(sums)
 print('Part2:', puzzle.answer_b)
 
-print(int(puzzle.answer_b) - int(puzzle.answer_a))
-
-print(sum(depths[i] > depths[i-3] for i in range(3, len(depths))))
+# this occurred to me later
+part_a, part_b = (sum(a < b for a, b in zip(depths, depths[i:])) for i in (1, 3))
+print(part_a, part_b)
