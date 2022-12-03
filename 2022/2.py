88c1818e262a648e04141c41554906fbf7fd5ffe
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2022-12-03 00:23:29 -0500

    2022 day 2 cleaned up.

--- 2022/2.py
+++ 2022/2.py
@@ -15,44 +15,39 @@ plays = list(map(str.split, input_data.split('\n')))
 
 
 def get_score(them, you):
-    return points[you] + (6 if to_win[them] == you else 3 if to_draw[them] == you else 0)
-    # if to_win[them] == you:
-    #     return 6 + points[you]
-    # elif to_lose[them] == you:
-    #     return points[you]
-    # else:
-    #     return 3 + points[you]
+    # return points[you] + (6 if to_win[them] == you else 3 if to_draw[them] == you else 0)
+    if to_win[them] == you:
+        return 6 + points[you]
+    elif to_lose[them] == you:
+        return points[you]
+    else:
+        return 3 + points[you]
 
 
 score = sum(get_score(*play) for play in plays)
-#
-# score = 0
-# for line in input_data.split('\n'):
-#     him, you = line.split()
-#     # print(score, him, you, end='')
-#     if win[you] == him:
-#         score += 6
-#     elif draw[you] == him:
-#         score += 3
-#     score += shapes[you]
-#     # print('', score)
+
+score = 0
+for them, you in plays:
+    if to_win[them] == you:
+        score += 6
+    elif to_draw[them] == you:
+        score += 3
+    score += points[you]
 
 puzzle.answer_a = score
 print('Part1:', puzzle.answer_a)
 
-correct_plays = {'X': to_lose, 'Y': to_draw, 'Z': to_win}
-score = sum(get_score(them, correct_plays[you][them]) for them, you in plays)
+# correct_plays = {'X': to_lose, 'Y': to_draw, 'Z': to_win}
+# score = sum(get_score(them, correct_plays[strategy][them]) for them, strategy in plays)
 
-# score = 0
-#
-# for line in input_data.split('\n'):
-#     him, you = line.split()
-#     if you == 'X':
-#         score += shapes[to_lose[him]]
-#     elif you == 'Y':
-#         score += 3 + shapes[to_draw[him]]
-#     else:
-#         score += 6 + shapes[to_win[him]]
-#
+score = 0
+for them, strategy in plays:
+    if strategy == 'X':
+        score += get_score(them, to_lose[them])
+    elif strategy == 'Y':
+        score += get_score(them, to_draw[them])
+    else:
+        score += get_score(them, to_win[them])
+
 puzzle.answer_b = score
 print('Part2:', puzzle.answer_b)
