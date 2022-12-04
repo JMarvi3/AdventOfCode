4eb64625b795e5dd8739412287a43f98be311115
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2022-12-04 00:26:16 -0500

    2022 day 4 solved.

---
+++ 2022/4.py
@@ -0,0 +1,22 @@
+from current_puzzle import current_puzzle
+import aoc_lib
+
+puzzle = current_puzzle()
+print(puzzle.title)
+input_data = puzzle.input_data
+# input_data  = open('4.example').read()
+
+count = 0
+part2_count = 0
+for line in input_data.split('\n'):
+    s = line.split(',')
+    a, b, c, d = map(int, s[0].split('-') + s[1].split('-'))
+    if (a <= c and d <= b) or (c <= a and b <= d):
+        count += 1
+    if not ((a < d and c > b) or (c < b and d < a)):
+        part2_count += 1
+
+puzzle.answer_a = count
+print('Part1:', puzzle.answer_a)
+puzzle.answer_b = part2_count
+print('Part2:', puzzle.answer_b)
