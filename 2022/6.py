825907730be9f4fddd5406f8643884f72c5b4173
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2022-12-06 00:20:52 -0500

    2022 day 6 cleaned up.

--- 2022/6.py
+++ 2022/6.py
@@ -7,12 +7,15 @@ input_data = puzzle.input_data
 # input_data = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 
 part1_answer = part2_answer = None
-for i in range(len(input_data)):
-    if part1_answer is None and i >= 4 and len(set(input_data[i - 4:i])) == 4:
+
+for i in range(4, len(input_data)):
+    if len(set(input_data[i-4:i])) == 4:
         part1_answer = i
-    if part2_answer is None and i >= 14 and len(set(input_data[i - 14:i])) == 14:
+        break
+
+for i in range(max(14, part1_answer), len(input_data)):
+    if len(set(input_data[i - 14:i])) == 14:
         part2_answer = i
-    if part1_answer and part2_answer:
         break
 
 puzzle.answer_a = part1_answer
