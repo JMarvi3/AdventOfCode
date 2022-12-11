d2088a8f6d9366ea50771bcbc904eb482c6a5eeb
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2022-12-11 11:39:08 -0500

    2022 day 11 cleaned up.

--- 2022/11.py
+++ 2022/11.py
@@ -1,48 +1,37 @@
-import re
 from copy import deepcopy
-
+from operator import mul, add
 from current_puzzle import current_puzzle
 import aoc_lib
 
 
 class Monkey:
+    monkeys = dict()
+    common_multiple = 1
+
     def __init__(self, text):
         text = text.splitlines()
         self.number = aoc_lib.findall('{:d}', text[0])[0]
         self.items = aoc_lib.findall('{:d}', text[1])
         op = text[2].split()
-        self.op = op[-2], op[-1]
-        self.test = int(text[3].split()[-1])
-        self.true = int(text[4].split()[-1])
-        self.false = int(text[5].split()[-1])
+        self.operator = mul if op[-2] == '*' else add
+        self.operand = op[-1] if op[-1] == 'old' else int(op[-1])
+        self.test = aoc_lib.findall('{:d}', text[3])[0]
+        self.true = aoc_lib.findall('{:d}', text[4])[0]
+        self.false = aoc_lib.findall('{:d}', text[5])[0]
         self.count = 0
+        Monkey.monkeys[self.number] = self
+        Monkey.common_multiple *= self.test
 
-    def do_stuff(self, monkeys: dict, worry_mitigation=None):
-        # print(monkey.number)
+    def do_stuff(self, part2=False):
         for item in self.items:
-            # print('item', item)
             self.count += 1
-            right = int(self.op[1]) if self.op[1] != 'old' else item
-            if self.op[0] == '*':
-                # print('multiplied by', right)
-                item *= right
-            else:
-                # print('increased by', right)
-                item += right
-            if worry_mitigation is None:
+            item = self.operator(item, item if self.operand == 'old' else self.operand)
+            if not part2:
                 item //= 3
-            else:
-                item %= worry_mitigation
-            if item % self.test == 0:
-                # print('passed', item, 'to', self.true)
-                monkeys[self.true].items.append(item)
-            else:
-                # print('passed', item, 'to', self.false)
-                monkeys[self.false].items.append(item)
+            item %= Monkey.common_multiple
+            destination = self.true if item % self.test == 0 else self.false
+            Monkey.monkeys[destination].items.append(item)
         self.items = []
-
-    def __repr__(self):
-        return f"{self.number}: ({self.count}) {self.items}"
 
 
 puzzle = current_puzzle()
@@ -50,30 +39,23 @@ print(puzzle.title)
 input_data = puzzle.input_data
 # input_data = open('11.example').read()
 
-worry = False
-monkeys = dict()
-for monkey in input_data.split('\n\n'):
-    node = Monkey(monkey)
-    monkeys[node.number] = node
-
-common = 1
-for monkey in monkeys.values():
-    common *= monkey.test
-
+monkeys = [Monkey(chunk) for chunk in input_data.split('\n\n')]
 part2_monkeys = deepcopy(monkeys)
 
 for i in range(20):
-    for monkey in monkeys.values():
-        monkey.do_stuff(monkeys)
+    for monkey in monkeys:
+        monkey.do_stuff()
 
-counts = sorted(monkey.count for monkey in monkeys.values())
+counts = sorted(monkey.count for monkey in monkeys)
 puzzle.answer_a = counts[-2] * counts[-1]
 print('Part1:', puzzle.answer_a)
 
-for i in range(10000):
-    for monkey in part2_monkeys.values():
-        monkey.do_stuff(part2_monkeys, common)
+Monkey.monkeys = {monkey.number: monkey for monkey in part2_monkeys}
 
-counts = sorted(monkey.count for monkey in part2_monkeys.values())
+for i in range(10000):
+    for monkey in part2_monkeys:
+        monkey.do_stuff(part2=True)
+
+counts = sorted(monkey.count for monkey in part2_monkeys)
 puzzle.answer_b = counts[-2] * counts[-1]
 print('Part2:', puzzle.answer_b)
