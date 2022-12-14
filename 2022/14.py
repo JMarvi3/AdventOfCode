b9303088d7add89648a3746e48d8b1a786bfe37e
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2022-12-14 04:07:11 -0500

    2022 day 14 cleaned up.

--- 2022/14.py
+++ 2022/14.py
@@ -1,41 +1,10 @@
-from copy import deepcopy
-
 from current_puzzle import current_puzzle
-import aoc_lib
 
-puzzle = current_puzzle()
-print(puzzle.title)
-input_data = puzzle.input_data
-# input_data = open('14.example').read()
-
-max_y = 0
-grid = dict()
-lines = []
-for line in input_data.splitlines():
-    coords = []
-    for coord in line.split(' -> '):
-        coords.append(tuple(map(int, coord.split(','))))
-    start = coords[0]
-    for end in coords[1:]:
-        move = end
-        if start > move:
-            start, move = move, start
-        x, y = start
-        if x == move[0]:
-            for y in range(start[1], move[1]+1):
-                grid[(x, y)] = '#'
-        else:
-            for x in range(start[0], move[0]+1):
-                grid[(x, y)] = '#'
-        start = end
-    lines.append(coords)
-min_x, max_x = min(g[0] for g in grid), max(g[0] for g in grid)
-min_y, max_y = min(g[1] for g in grid), max(g[1] for g in grid)
 
 def draw_grid(grid):
-    for y in range(min_y-5, max_y+3):
-        print(''.join(grid.get((x,y), '.') for x in range(min_x-10, max_x+10)))
-    print('------------------------')
+    for y in range(max_y+3):
+        print(''.join(grid.get((x, y), ' ') for x in range(min_x, max_x+1)))
+
 
 def do_sand(maximum):
     sand = (500, 0)
@@ -44,34 +13,61 @@ def do_sand(maximum):
         if sand[-1] == maximum:
             break
         possible_move = False
-        for pt in [(0,1), (-1,1), (1,1)]:
-            new_pt = aoc_lib.add_pts(sand, pt)
+        for pt in [(0, 1), (-1, 1), (1, 1)]:
+            new_pt = (sand[0] + pt[0], sand[1] + pt[1])
             if new_pt not in grid:
                 sand = new_pt
                 possible_move = True
                 break
     return sand
 
-moves = 0
+
+puzzle = current_puzzle()
+print(puzzle.title)
+input_data = puzzle.input_data
+# input_data = open('14.example').read()
+
+grid = dict()
+max_y = -float('inf')
+for line in input_data.splitlines():
+    coords = []
+    for coord in line.split(' -> '):
+        coords.append(tuple(map(int, coord.split(','))))
+    start = coords[0]
+    for end in coords[1:]:
+        max_y = max(max_y, start[1], end[1])
+        move = end
+        if start > move:
+            start, move = move, start
+        x, y = start
+        if x == move[0]:
+            for y in range(start[1], move[1] + 1):
+                grid[(x, y)] = '#'
+        else:
+            for x in range(start[0], move[0] + 1):
+                grid[(x, y)] = '#'
+        start = end
+
+min_x, max_x = 500 - max_y - 2, 500 + max_y + 2
+grid.update({(x, max_y+2): '#' for x in range(min_x, max_x+1)})
+empty_grid_size = len(grid)
+
 while True:
     sand = do_sand(max_y)
-    grid[sand] = 'o'
-    moves += 1
     if sand[-1] >= max_y:
         break
+    grid[sand] = '.'
 
-puzzle.answer_a = moves - 1
+
+puzzle.answer_a = len(grid) - empty_grid_size
 print('Part1:', puzzle.answer_a)
 
-moves -= 1
-del grid[sand]
 while True:
-    sand = do_sand(max_y+1)
+    sand = do_sand(max_y + 1)
     grid[sand] = 'o'
-    moves += 1
     if sand == (500, 0):
         break
 
-puzzle.answer_b = moves
+# draw_grid(grid)
+puzzle.answer_b = len(grid) - empty_grid_size
 print('Part2:', puzzle.answer_b)
-# print(puzzle.incorrect_answers_b)
