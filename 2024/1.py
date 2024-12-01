ed73ecb264d658ec14cf2d3ca72284616554ceb5
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2024-12-01 16:52:21 -0500

    2024 Day 1 refactored.

--- 2024/1.py
+++ 2024/1.py
@@ -1,24 +1,21 @@
 from current_puzzle import current_puzzle
+from collections import Counter
 
 puzzle = current_puzzle()
 print(puzzle.title)
 input_data = puzzle.input_data
 # input_data = open('1.example').read()
 
-data = [list(map(int, line.split())) for line in input_data.splitlines()]
-data = list(zip(*data))
-data = list(map(sorted, data))
-data_2 = data[::]
-data = list(zip(*data))
+data = [map(int, line.split()) for line in input_data.splitlines()]
+data = list(map(sorted, zip(*data)))
+total_distance = sum(abs(a-b) for a, b in zip(*data))
 
-total_distance = sum(map(lambda elem: abs(elem[0]-elem[1]), data))
 print(total_distance)
-# puzzle.answer_a = total_distance
+puzzle.answer_a = total_distance
 print('Part1:', puzzle.answer_a)
 
-similarity_score = 0
-for num in data_2[0]:
-    similarity_score += num * data_2[1].count(num)
+counts = Counter(data[1])
+similarity_score = sum(num*counts[num] for num in data[0])
 
 print(similarity_score)
 puzzle.answer_b = similarity_score
